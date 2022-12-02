8e6b2a59c9bd98a9c86452ef5260c97eb86abf5a
Temperz87 <58375877+Temperz87@users.noreply.github.com>
2022-12-02 00:22:47 -0500

    Day 2 and refactor
    Completed P1 in 12:48 with a rank of 4581, P2 in 19:30 with a rank of 4345

--- Aoc 2022 College is Stressful Year/Program.cs
+++ Aoc 2022 College is Stressful Year/Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     private static void Main(string[] args)
     {
-        Day1();
+        Day2P2();
     }
     
     private static void PrintAnswer(object text)
@@ -26,7 +26,7 @@ class Program
         Console.ForegroundColor = ConsoleColor.White;
     }
 
-    private static void Day1()
+    private static void Day1() // Completed P1 in 2:42 with a rank of 928, P2 in 4:81 wiht a rank of 921
     {
         string[] input = InputGetter.GetStringInputs();
         List<int> allCalories = new List<int>();
@@ -51,6 +51,75 @@ class Program
         allCalories.Sort();
         //PrintAnswer(max); for part 1
         PrintAnswer(allCalories[allCalories.Count - 1] + allCalories[allCalories.Count - 2] + allCalories[allCalories.Count - 3]); // for part 2
+    }
+
+    public static void Day2P1() // Completed P1 in 12:48 with a rank of 4581, P2 in 19:30 with a rank of 4345
+    {
+        string[] inputs = InputGetter.GetStringInputs();
+        int score = 0;
+        foreach (string s in inputs)
+        {
+            if (s[0] == 'C' && s[2] == 'X')
+                score += 7;
+            else if (s[0] == 'A' && s[2] == 'Y')
+                score+=8;
+            else if (s[0] == 'B' && s[2] == 'Z')
+                score+=9;
+            else if (s[0] == 'A' && s[2] == 'X')
+                score += 4;
+            else if (s[0] == 'B' && s[2] == 'Y')
+                score += 5;
+            else if (s[0] == 'C' && s[2] == 'Z')
+                score += 6;
+            else // Copilot generated this else at 12:10 am and I have about 2 minutes to wait cuz I spammed answers, is this cheating?
+            {
+                if (s[2] == 'X')
+                    score += 1;
+                else if (s[2] == 'Y')
+                    score += 2;
+                else if (s[2] == 'Z')
+                    score += 3;
+            }
+        }
+        PrintAnswer(score);
+    }
+
+
+    public static void Day2P2() // Honestly was easier in the moment to split this into two functions
+    {
+        string[] inputs = InputGetter.GetStringInputs();
+        int score = 0;
+        foreach (string s in inputs)
+        {
+            if (s[2] == 'X')
+            {
+                if (s[0] == 'A')
+                    score += 3;
+                else if (s[0] == 'B')
+                    score += 1;
+                else
+                    score += 2;
+            }
+            else if (s[2] == 'Y')
+            {
+                if (s[0] == 'A')
+                    score += 4;
+                else if (s[0] == 'B')
+                    score += 5;
+                else
+                    score += 6;
+            }
+            else
+            {
+                if (s[0] == 'A')
+                    score += 8;
+                else if (s[0] == 'B')
+                    score += 9;
+                else
+                    score += 7;
+            }
+        }
+        PrintAnswer(score);
     }
 }
 
